63f9a31616181b8da1ab18756718c5f818297d78
ramanetka <ramanetka@gmail.com>
2019-03-02 21:07:49 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// " use strict";
+
+// var name = "nika";
+// let number = 5;
+
+// let person = {
+//     i: "nika",
+//     u: "olo"
+// }
+// let errey = [1,2]
+
+// let answ = prompt("gerg?","yes")
+
+// console.log(typeof(number));
+
+let answ = +prompt("gerg?","ye");
+
+// console.log(typeof(answ));
+console.log(4 + 4)
